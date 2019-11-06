357e8f131378a7d14494f5f6f001c7191855f929
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-06 20:56:37 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,10 @@ def bonus
    }
   }
 
-  epic_tragedy[:status] = "dead"
-  epic_tragedy[:status]= "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status]= "dead"
 
-  
+end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
